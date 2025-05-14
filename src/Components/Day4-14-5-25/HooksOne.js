0d0fa27044faf20a540340509c8e156d99b23b79
Srinivas <lingampellisrinivas3036@gmail.com>
2025-05-14 12:34:42 +0530

    useEffect is modify data while ComponentDidMount but useState is not like that

--- src/Components/Day4-14-5-25/HooksOne.js
+++ src/Components/Day4-14-5-25/HooksOne.js
@@ -6,10 +6,13 @@ function Hooks()
 
     const [count,setCount]=useState(0);
 
+    useEffect(()=>
+    {
+        document.title=`You Clicked ${count}`;
+    })
+
     return <div>
-        <button onClick={()=>{setCount(count+1),
-            document.title=`you clicked ${count}`
-        }}  >
+        <button onClick={()=>{setCount(count+1)}}  >
           COunt  {count}
         </button>
     </div>
